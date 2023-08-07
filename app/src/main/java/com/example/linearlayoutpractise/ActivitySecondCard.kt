abc042a626c765ea6c2d34fdb0c167c4196f73de
carlosmedhat <kirolosmedhat274@gmail.com>
2023-08-07 12:32:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/linearlayoutpractise/ActivitySecondCard.kt
@@ -0,0 +1,11 @@
+package com.example.linearlayoutpractise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySecondCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_card)
+    }
+}
